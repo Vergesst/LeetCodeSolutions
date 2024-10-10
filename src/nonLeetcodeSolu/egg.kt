1e0d0fdf65e9b63d5bda65675a50791300dc3066
Vergesst <3224171967@qq.com>
2024-10-10 14:15:25 +0800

    a new Java solution

---
+++ src/nonLeetcodeSolu/egg.kt
@@ -0,0 +1,43 @@
+package nonLeetcodeSolu
+
+fun distributeEgg(eggs: Int, baskets: Int): List<List<Int>> {
+    val result = mutableListOf<List<Int>>()
+    val path = mutableListOf<Int>()
+
+    fun backtrack(eggs: Int, baskets: Int) {
+        fun bigger (a: Int, b: List<Int>): Boolean {
+            for(x in b) {
+                if (a > x) {
+                    return false
+                }
+            }
+            return true
+        }
+        if (baskets == 0) {
+            if (eggs == 0) {
+                result.add(path.toList())
+            }
+            return
+        }
+        val end = eggs / baskets
+        for(i in eggs downTo end) {
+            if(bigger(i, path)) {
+                path.add(i)
+                backtrack(eggs - i, baskets - 1)
+                path.removeAt(path.size - 1)
+            }
+        }
+    }
+
+    backtrack(eggs, baskets)
+    return result
+}
+
+fun main() {
+    val eggs = readln().toInt()
+    val baskets = readln().toInt()
+    val result = distributeEgg(eggs, baskets)
+    for (r in result) {
+        println(r.toString())
+    }
+}
